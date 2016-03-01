916455f1c5ad8e0d8bcce7662119179f4eb7e7a6
Martin Stanek <info@martinstanek.cz>
2016-03-01 11:24:05 +0100

    Handle object as data, line graph labels

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   lineGraph: require('./lib/line-graph'),
   barChart: require('./lib/bar-chart'),
   findMin: require('./lib/find-min'),
-  findMax: require('./lib/find-max')
+  findMax: require('./lib/find-max'),
+  objectToArray: require('./lib/object-to-array')
 };
 
